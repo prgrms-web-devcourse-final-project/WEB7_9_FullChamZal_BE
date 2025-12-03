bd13dda0d8bf3ad6365395ffcd36079222261182
aovor <0hy2s2@naver.com>
2025-12-03 22:14:32 +0900

    init(backend): 기본 프로젝트 설정

---
+++ src/main/java/back/fcz/FczApplication.java
@@ -0,0 +1,13 @@
+package back.fcz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FczApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FczApplication.class, args);
+    }
+
+}
