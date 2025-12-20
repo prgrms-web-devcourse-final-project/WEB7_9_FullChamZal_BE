7fea6b255b227d40ccc3104cf4648f63704e71ec
aovor <0hy2s2@naver.com>
2025-12-20 20:05:23 +0900

    fix(member): MemberScheduler 회원 탈퇴 익명화 로직 수정

--- src/main/java/back/fcz/domain/capsule/repository/CapsuleRecipientRepository.java
+++ src/main/java/back/fcz/domain/capsule/repository/CapsuleRecipientRepository.java
@@ -30,7 +30,8 @@ public interface CapsuleRecipientRepository extends JpaRepository<CapsuleRecipie
             pcr.recipientPhoneHash = CONCAT('DELETED_', pcr.id)
         WHERE pcr.recipientPhoneHash = :phoneHash
     """)
-    void anonymizeByRecipientPhoneHash(@Param("phoneHash") String phoneHash);
+    void anonymizeByRecipientPhoneHash(@Param("phoneHash") String phoneHash,
+                                       @Param("memberId") Long memberId);
 
     Optional<CapsuleRecipient> findByCapsuleId_CapsuleIdAndRecipientPhoneHash(Long capsuleId, String phoneHash);
     // phoneHash를 가지는 수신자 리스트 조회. JOIN을 이용하여 Capsule 테이블도 같이 조회
